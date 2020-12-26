884b254617196679a028f32f43556f42cbec608b
jasont41 <43120995+jasont41@users.noreply.github.com>
2020-12-26 14:48:02 -0800

    Different method to flash LED's

--- test project/blink.c
+++ test project/blink.c
@@ -4,16 +4,31 @@
 /**
  * blink.c
  */
+
+#define LED1    BIT3
+#define B1      BIT1
 void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= 0x01;					// configure P1.0 as output
+	P2OUT |= LED1;
+	P2DIR = LED1;
 
-	volatile unsigned int i;		// volatile to prevent optimization
+
+	for(;;){
+	    if((P2IN & B1) == 0){
+	        P2OUT &= ~LED1;
+	    }
+	    else{
+	        P2OUT |= LED1;
+	    }
+	}
+
+	/*volatile unsigned int i;		// volatile to prevent optimization
 
 	while(1)
 	{
 		P1OUT ^= 0x01;				// toggle P1.0
+
 		for(i=10000; i>0; i--);     // delay
-	}
+	}*/
 }
