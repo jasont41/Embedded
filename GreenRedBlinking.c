e5b5339c8d72a374641bde2af40d2f903499bc9f
jasont41 <43120995+jasont41@users.noreply.github.com>
2020-12-27 19:37:58 -0800

    Create GreenRedBlinking.c

---
+++ GreenRedBlinking.c
@@ -0,0 +1,25 @@
+#include <msp430.h>				
+
+
+/**
+ * blink.c
+ */
+int main(void) {
+
+
+        volatile int i; //Force the compliler to not optimize the variable 'i'
+        WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+        P1DIR|=BIT0|BIT6; //Set P1.0 and P1.6 as output pin
+        while(1) //Keep toggling the LEDs forever
+        {
+            P1OUT|=BIT0; //Turn on red LED
+            P1OUT&=~BIT6; //Turn off green LED
+            for(i=20000;i>0;i--); //Delay
+            P1OUT|=BIT6; //Turn on green LED
+            P1OUT&=~BIT0; //Turn off red LED
+            for(i=20000;i>0;i--); //Delay
+        }
+
+        return 0;
+
+}
